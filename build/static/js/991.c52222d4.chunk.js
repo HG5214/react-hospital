9ced785526ffb3ec240e04e576abe45ad7128315
hgf <18579070293@163.com>
2023-07-31 11:48:47 +0800

    '打包项目'

---
+++ build/static/js/991.c52222d4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksyt_admin=self.webpackChunksyt_admin||[]).push([[991],{4991:function(n,s,e){e.r(s);e(2791);var a=e(5897),h=e(3383),t=e(4740),c=e(746),i=e(184);s.default=function(){var n=(0,t.C)(c.dy),s=(0,a.$)(["app"]).t;return(0,i.jsxs)(h.Z,{style:{minHeight:"calc(100vh - 64px)"},children:[(0,i.jsx)("h2",{children:s("dashboard")}),(0,i.jsxs)("h2",{children:["\u7528\u6237\u540d: ",n.name]})]})}}}]);
+//# sourceMappingURL=991.c52222d4.chunk.js.map
